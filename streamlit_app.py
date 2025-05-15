cc0a8215589964892bd848165b925521c5978051
Adam-art1 <blank7921@gmail.com>
2025-05-15 14:41:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Punya gweh 😋")
+st.image("./17472912470587405037618347906961.jpg", width=200)
+st.write("\n")
+st.subheader("Adam nugget")
+st.write("PENTOLLLLL")
